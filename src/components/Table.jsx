05da2bfe6af586f71b7bf83d457402b3c870871c
adarsh291201 <116202179+adarsh291201@users.noreply.github.com>
2024-11-27 20:52:06 +0530

    worked the responsivesness

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,42 +1,43 @@
 import './Table.css'
 const Table = ({ columns, data, onEdit, onDelete }) => (
-  <table className="table-auto w-full border-collapse border border-gray-200">
-    <thead>
-      <tr>
-        {columns.map((col) => (
-          <th key={col} className="border border-gray-200 px-4 py-2">
-            {col}
-          </th>
-        ))}
-        <th className="border border-gray-200 px-4 py-2">Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((row, index) => (
-        <tr key={index} className="text-center">
+  <div className='overflow-auto'>
+
+    <table className="table-auto w-full border-collapse border border-gray-200">
+      <thead>
+        <tr>
           {columns.map((col) => (
-            <td key={col} className="border border-gray-200 px-4 py-2">
-              {row[col.toLowerCase()]}
-            </td>
+            <th key={col} className="border border-gray-200 px-4 py-2">
+              {col}
+            </th>
           ))}
-          <td className="border border-gray-200 px-4 py-2">
-            <button className='edit-btn'
-              onClick={() => onEdit(row)}
-              // className="text-blue-500 mr-2"
-            >
-              Edit
-            </button>
-            <button className='delete-btn'
-              onClick={() => onDelete(row.id)}
-              // className="text-red-500"
-            >
-              Delete
-            </button>
-          </td>
+          <th className="border border-gray-200 px-4 py-2">Actions</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index} className="text-center">
+            {columns.map((col) => (
+              <td key={col} className="border border-gray-200 px-4 py-2">
+                {row[col.toLowerCase()]}
+              </td>
+            ))}
+            <td className="border border-gray-200 px-4 py-2">
+              <button className='edit-btn'
+                onClick={() => onEdit(row)}
+              >
+                Edit
+              </button>
+              <button className='delete-btn'
+                onClick={() => onDelete(row.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 
 export default Table;
